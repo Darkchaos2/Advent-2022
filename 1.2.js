3ef2b764ae83ca59b73820267efe305cde20285c
Darkchaos <louis_james1998@hotmail.co.uk>
2022-12-01 22:22:59 +0000

    Formatting

--- 1.2.js
+++ 1.2.js
@@ -2,4 +2,4 @@ document.querySelector('pre').innerText.split("\n").reduce((acc, e) => {
     if (e == "") return [...acc, 0];
     acc[acc.length - 1] += parseInt(e);
     return acc;
-}, [0]).sort((a, b) => a-b).slice(-3).reduce((acc, e) => acc += e, 0)
+}, [0]).sort((a, b) => a - b).slice(-3).reduce((acc, e) => acc += e, 0)
