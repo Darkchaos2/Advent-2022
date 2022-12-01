1d875d46ac5fe217d89d5ce2d203f8540e514b83
Darkchaos <louis_james1998@hotmail.co.uk>
2022-12-01 22:17:05 +0000

    Day 1

---
+++ 1.1.js
@@ -0,0 +1,28 @@
+currMax = 0;
+curr = 0;
+currElf = 0;
+currMaxElf = 0;
+loopID = 0;
+maxLoopID = 0;
+
+document.querySelector('pre').innerText.split("\n").forEach(e => {
+    loopID++;
+    if (e == "") {
+        console.log(curr);
+        console.log(currMax);
+        if (curr > currMax) {
+            currMax = curr;
+            currMaxElf = currElf;
+            maxLoopID = loopID;
+        }
+        curr = 0;
+        currElf++;
+        return;
+    }
+
+    curr += parseInt(e);
+})
+
+console.log(currMax);
+console.log(currMaxElf);
+console.log(maxLoopID);
---
+++ 1.2.js
@@ -0,0 +1,5 @@
+document.querySelector('pre').innerText.split("\n").reduce((acc, e) => {
+    if (e == "") return [...acc, 0]
+    acc[acc.length - 1] += parseInt(e)
+    return acc
+}, [0]).sort((a,b)=>a-b).slice(-3).reduce((acc, e) => {return acc += e}, 0)
