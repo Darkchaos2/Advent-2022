d7bc374efa9e2ef960b0fcce995846207569bcf2
Darkchaos <louis_james1998@hotmail.co.uk>
2022-12-04 22:10:40 +0000

    Day 4

---
+++ 4/4.1.js
@@ -0,0 +1,1 @@
+document.querySelector('pre').innerText.split("\n").filter(e => e != "").map(line => line.split(",").map(allotment => allotment.split("-").map(number => parseInt(number)))).reduce((acc, line) => acc += (line[0][0] >= line[1][0] && line[0][1] <= line[1][1] || line[1][0] >= line[0][0] && line[1][1] <= line[0][1]), 0)
