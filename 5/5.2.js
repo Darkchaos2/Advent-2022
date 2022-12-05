03eed5147bf61dccd536887fca997c5d86ac3227
Darkchaos <louis_james1998@hotmail.co.uk>
2022-12-05 18:11:34 +0000

    Day 5
    Looking a bit dirty

---
+++ 5/5.2.js
@@ -0,0 +1,18 @@
+var [oldStack, inst] = document.querySelector('pre').innerText.split("\n\n").map(e => e.split('\n').filter(e2 => e2 != ""))
+var stack = oldStack.map(e => [])
+oldStack.pop();
+
+oldStack.forEach(line => {
+    for(var i = 1; i < 35; i += 4) {
+        var char = line[i];
+        if (char != " ") stack[(i - 1) / 4].push(char)
+    }    
+})
+stack.unshift([])
+
+inst.forEach(e => {
+    var [match, quantity, from, to] = [...e.matchAll(/move ([0-9]+) from ([0-9]+) to ([0-9]+)/g)][0]
+    stack[to] = stack[from].splice(0, quantity).concat(stack[to].concat())
+})
+
+stack.reduce((acc, e) => acc += e?.[0])
